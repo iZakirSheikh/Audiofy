5a2eaa3a9a27d5837b758b9e33f497e319ad410b
Zakir Sheikh <sheikhzakirahmad@gmail.com>
2024-08-12 11:02:04 +0530

    [FIX] Rolled back Unity to version 4.12.1. The recent update caused the APK size to triple, leading to significant issues. After 32 hours of testing and investigation, I identified the update as the root cause and reverted to the previous version.
    [FEAT] Refactored the workflow file for better clarity. Renamed steps to more accurately describe their purpose and added Gradle caching, resulting in a 2x faster build process.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.prime.player"
         minSdk = 21
         targetSdk = 34
-        versionCode = 116
-        versionName = "2.15.0-beta02"
+        versionCode = 123
+        versionName = "2.15.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
         //Load secrets into BuildConfig
