dde46b80c777f5ee6baf6fb800a4755659dd7177
sheikh <sheikhzakirahmad@gmail.com>
2025-06-23 12:20:10 +0530

    [INFRA] Update dependencies and app version.
    - **Dependencies (`gradle/libs.versions.toml`):**
        - `agp` (Android Gradle Plugin) updated from `8.10.0` to `8.10.1`.
        - `kotlin` updated from `2.1.20` to `2.1.21`.
        - `compose` updated from `1.8.1` to `1.8.3`.
        - `room` updated from `2.7.1` to `2.7.2`.
    - **App Version (`app/build.gradle.kts`):**
        - `versionCode` incremented from `206` to `207`.
        - `versionName` changed from `3.8.4` to `3.8.5`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,8 @@ android {
         applicationId = "com.prime.player"
         minSdk = 21
         targetSdk = 35
-        versionCode = 206
-        versionName = "3.8.4"
+        versionCode = 207
+        versionName = "3.8.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
         // init different config fields.
