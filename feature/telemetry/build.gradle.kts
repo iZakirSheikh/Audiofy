f12bfdbe18cfb782db0eb56e0be27ca54cc9c782
Zakir Sheikh <sheikhzakirahmad@gmail.com>
2025-07-26 14:39:03 +0530

    [REFACTOR]: Simplify Proguard configuration in feature modules
    This commit simplifies the Proguard configuration in the `codex` and `telemetry` feature modules.
    
    The `getDefaultProguardFile("proguard-android-optimize.txt")` call has been removed from the `proguardFiles` configuration in the `release` build type for both modules. Only the custom `proguard-rules.pro` file is now specified.

--- feature/telemetry/build.gradle.kts
+++ feature/telemetry/build.gradle.kts
@@ -14,9 +14,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
+            proguardFiles("proguard-rules.pro")
         }
     }
     compileOptions {
